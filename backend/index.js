308f504613b060f8dca7289ff993d220aa01bab4
zakaria-root <elmourtazakezia@gmail.com>
2021-11-24 02:09:31 +0100

    start the server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express  = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => { console.log('the server is started ' + PORT) });
